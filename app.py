ba99859f2ef58d64e2817c1aaa33377994b6aca2
vAndrean-drag <andrean.sihombing83@gmail.com>
2023-10-23 08:14:43 +0700

    Remove randomforestclassifier package

--- app.py
+++ app.py
@@ -2,8 +2,6 @@
 import streamlit as st
 import pickle
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
-
 
 # set page title and icon
 st.set_page_config(page_title="Heart Disease Detection", page_icon="🫀")
